4924b03579cc848a8256764a1be6b39e9b6c78a5
Julian Garcia <julian.garcia.leoni@gmail.com>
2018-09-28 18:08:48 +0100

    Link wakatime api to chart

--- about/views.py
+++ about/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+import requests, json
 
 def index(request):
-    return render(request, 'index.html')
+    languages = requests.get('https://wakatime.com/share/@c3419dbf-0038-499d-9d18-532e0b87876f/5936d31f-1414-404c-a67b-98f15a6dbb51.json')
+    langs_list = json.loads(languages.content)['data']
+
+    langs_dict = {}
+    for lang in langs_list:
+        langs_dict[lang['name']] = lang['percent']
+
+    return render(request, 'index.html', {'languages': json.dumps(langs_dict)})
