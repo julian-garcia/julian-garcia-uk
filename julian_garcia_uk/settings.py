db10ed30abb76822594200339f1dab34e442b498
Julian Garcia <julian.garcia.leoni@gmail.com>
2018-10-01 01:18:17 +0100

    Github repo api, home page layout

--- julian_garcia_uk/settings.py
+++ julian_garcia_uk/settings.py
@@ -146,8 +146,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = '/static/'
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = '/media/'
---
+++ static/scripts/github.js
@@ -0,0 +1,17 @@
+async function githubRepos() {
+  let github_repos_response = await fetch('https://api.github.com/users/julian-garcia/repos');
+  let github_repos = await github_repos_response.json();
+  let github_div = document.querySelector('.github');
+  console.log(github_repos);
+
+  github_repos.forEach(function(repo) {
+    let repo_link = document.createElement('a');
+    repo_link.textContent = repo.name;
+    repo_link.href = repo.html_url;
+    repo_link.setAttribute('target', '_blank');
+    repo_link.className = 'github-button button is-light is-rounded is-medium';
+    github_div.append(repo_link);
+  });
+}
+
+githubRepos();
