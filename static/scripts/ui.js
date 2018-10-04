5411e0a7457f8b9fe0e4590349a6e906498d3826
Julian Garcia <julian.garcia.leoni@gmail.com>
2018-10-04 01:29:31 +0100

    Experience time line

--- static/scripts/ui.js
+++ static/scripts/ui.js
@@ -1,24 +1,25 @@
 const mobile_button_menu = document.getElementById('mobile-menu-button');
 const side_nav = document.querySelector('.side-nav');
-const page_title = document.title.replace('Julian Garcia - ','');
+const my_name = 'Julian Garcia';
+const page_title = document.title.replace(`${my_name} - `,'');
 
-mobile_button_menu.addEventListener('mouseup', function(e) {
-  side_nav.style.display = window.getComputedStyle(side_nav).display == 'none' ? 'block' : 'none';
-  e.preventDefault();
-});
-
-side_nav.addEventListener('click', function(e) {
+// On page load highlight the relevant navigation menu item based on the current page
+document.addEventListener('DOMContentLoaded', function(e) {
   Array.from(side_nav.children).forEach(function(nav_button) {
-    console.log(nav_button);
-    if (nav_button.textContent.trim() == page_title.trim()) {
+    if (nav_button.textContent.trim() == page_title.trim() ||
+        (page_title.trim() == my_name && nav_button.textContent.trim() == 'About Me')) {
       nav_button.classList.remove('inactive-border');
       nav_button.classList.add('active-border');
-      console.log(nav_button);
     } else {
       nav_button.classList.remove('active-border');
       nav_button.classList.add('inactive-border');
     }
   });
+});
+
+mobile_button_menu.addEventListener('mouseup', function(e) {
+  side_nav.style.display = window.getComputedStyle(side_nav).display == 'none' ? 'block' : 'none';
+  e.preventDefault();
 });
 
 /* Ensure that the menu is only hidden on mobile screen sizes, otherwise */
