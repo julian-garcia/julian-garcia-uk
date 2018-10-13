3a99429faa62b1773166d26352d50a18a95b0fdb
Julian Garcia <julian.garcia.leoni@gmail.com>
2018-10-13 16:00:42 +0100

    CV doc/pdf and links

--- julian_garcia_uk/settings.py
+++ julian_garcia_uk/settings.py
@@ -152,7 +152,7 @@ STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = STATIC_URL + 'media/'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
 
