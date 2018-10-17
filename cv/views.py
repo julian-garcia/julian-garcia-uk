1858226c019133c17eecb551784842c12c860433
Julian Garcia <julian.garcia.leoni@gmail.com>
2018-10-17 02:14:44 +0100

    Layout and CV adjustments

--- cv/views.py
+++ cv/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import CVSection
 
 def cv(request):
-    headline = CVSection.objects.all().order_by('section_sequence','-date_saved')[:1].get()
-    sections = CVSection.objects.all().order_by('section_sequence','-date_saved')[1:]
+    headline = CVSection.objects.all().order_by('section_sequence','-section_start_date')[:1].get()
+    sections = CVSection.objects.all().order_by('section_sequence','-section_start_date')[1:]
     return render(request, 'cv.html', {'headline': headline, 'sections': sections})
