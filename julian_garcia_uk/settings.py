c5605b1ce83ee71d37e2805eeb4c4f229d93f5f4
Julian Garcia <julian.garcia.leoni@gmail.com>
2018-10-27 02:01:23 +0100

    Notes body markdown

--- julian_garcia_uk/settings.py
+++ julian_garcia_uk/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'attending',
     'storages',
     'experience',
+    'markdownify',
 ]
 
 MIDDLEWARE = [
@@ -163,3 +164,8 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
+MARKDOWNIFY_WHITELIST_TAGS = [
+  'a',
+  'h1','h2','h3','p'
+]
